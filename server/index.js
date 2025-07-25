3cfc3a681cfd8d139ba3a4666130e5811e005b39
Tejas-Kharche <tejaskharche03@gmail.com>
2025-07-26 01:18:21 +0530

    Connected the Vercel frontend domain

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: ['https://promptify-mocha.vercel.app'], // your Vercel frontend domain
+  methods: 'GET,POST',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Routes
