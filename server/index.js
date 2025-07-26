15a3bdcf49dc75f18c20c477269cc33331ae80af
Tejas-Kharche <tejaskharche03@gmail.com>
2025-07-26 11:42:33 +0530

    Added auth route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+app.use('/auth', authRoutes);
+
 
 const playlistRoutes = require('./routes/playlistRoutes');
 
